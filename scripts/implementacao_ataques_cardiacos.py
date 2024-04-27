aaa9d2c23910481738b56eada65a71f33337a02f
Natan Cabral <102121764+Ntzim@users.noreply.github.com>
2024-04-27 20:09:58 -0300

    Update implementacao_ataques_cardiacos.py

--- scripts/implementacao_ataques_cardiacos.py
+++ scripts/implementacao_ataques_cardiacos.py
@@ -144,10 +144,10 @@ elif escolha_do_indicadores == 'Previsão de Ataques Cardiacos':
 
     #TITULO
     st.subheader('Sistema de Previsão de Ataques Cardiacos')
-    st.write('Com base nos resultados do exame, nosso sistema preve ataques cardiacos')
+    st.write('"Nossa plataforma analisa cuidadosamente os dados do exame para prever possíveis ataques cardíacos. Com base nos resultados, fornecemos uma avaliação precisa do risco de ataque cardiaco. Sua saúde é nossa prioridade, e estamos aqui para oferecer orientações para a prevenção e o cuidado."')
 
     #DESCRIÇÃO
-    st.write('Após preencher todos os dados, clique no botão abaixo para gerar o resultado')
+    st.write('Após fornecer todas as informações necessárias, clique no botão abaixo para gerar seu resultado personalizado.')
 
     #CHAMANDO FUNÇÃO
     df = get_user_data()
