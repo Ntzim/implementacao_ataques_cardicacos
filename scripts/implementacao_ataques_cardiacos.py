af92594f35336cf6ccbb5bc2cc8b72de2286cee1
Natan Cabral <102121764+Ntzim@users.noreply.github.com>
2024-05-08 19:56:13 -0300

    Update implementacao_ataques_cardiacos.py

--- scripts/implementacao_ataques_cardiacos.py
+++ scripts/implementacao_ataques_cardiacos.py
@@ -250,7 +250,7 @@ elif escolha_do_indicadores == 'Previsão de Ataques Cardiacos':
     previsão = modelo.predict_proba(df[variaveis_selecionadas])[:,1]*100
 
     #INSERINDO PREVISÂO NO DF
-    df ['Resultado'] = f'{previsão}(:.2f)'
+    df ['Resultado'] = f'{previsão:.2f}'
     
     #SETANDO VARIAVEIS PARA PLOTAR TABELA
     colunas = [
